ea9de0558e7617ead1dbed30178a765bd74b6145
Mehrob <shmehrob16@gmail.com>
2022-08-01 06:39:03 +0500

    Game

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -9,10 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+    var touches = 0 {
+        didSet {
+            touchLable.text = String("Touches: \(touches)")
+        }
     }
+    
+    func flipButton (emoji:String, button:UIButton){
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.01348185912, green: 0.7802809477, blue: 0.744160831, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
+        
+        
+    }
+    
+    let emojiCollection = ["🦊","🐰","🦊","🐰"]
+    
+    
+    
+    
+    
+    
+    
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBOutlet weak var touchLable: UILabel!
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+                
+        }
+        
+    }
+    
+    
 
 
 }
